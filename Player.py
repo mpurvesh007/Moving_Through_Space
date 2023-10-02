2d5951ecf2543a05bfe4285af04aa64da0af2b80
mpurvesh007 <mpurvesh007@gmail.com>
2023-10-02 12:31:58 +0100

    Player class

---
+++ Player.py
@@ -0,0 +1,17 @@
+###Submission by : Purvesh Mehta
+###Candidate No. : 260867
+"""
+    A simple class to to use the users name.
+"""
+
+
+class Player:
+    def __init__(self, name):
+        """
+            Constructor method.
+        :param name: For user name
+        """
+        self.name = name
+        self.inventory = []
+
+
